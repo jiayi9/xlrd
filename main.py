e1a76a61b6552af4a4b8823f4d509fea43597366
Jiayi Lu <215756347@qq.com>
2019-08-29 16:22:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from functions import read_to_list, insert_to_database, list_files_recur
 
+FILE_PATH = "//bosch.com/dfsrb/DfsCN/loc/Wx/Dept/TEF/60_MFE_Manufacturing_Engineering/06_Data_Analytics/01_Project/RBCD/cleanliness/DNOX/2.2 -SM-2.xlsx"
+
+L = read_to_list(FILE_PATH)
+
+insert_to_database(L)
+
+
+    
+file_paths, file_names = list_files_recur("C:/daten", format = '.xlsx')
