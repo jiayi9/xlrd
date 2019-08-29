d8fa9067c54de56fcab590a59b030d0311178a40
Jiayi Lu <215756347@qq.com>
2019-08-29 14:16:04 +0800

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,33 @@
+CREATE TABLE REPORTS (
+	ANALYSIS_NO varchar(255),
+	EXTRACTION_DEVICE varchar(255),
+	TEST_SPEC varchar(255),
+	STANDARD varchar(255),
+	COMPONENT_NO varchar(255),
+	BATCH_NO varchar(255),
+	MEDIA_SAMPLE varchar(255),
+	OTHERS varchar(255),
+	SAMPLE_NO varchar(255),
+	SHIPPING_PACKAGE varchar(255),
+	EXTRACTION_DATE varchar(255),
+	FILTERING_DRYING varchar(255),
+	MICROSCOPE varchar(255),
+	SOFTWARE_VERSION varchar(255),
+	CALIBRATION varchar(255),
+	FILTER_SIZE varchar(255),
+	ANALYZE_SIZE varchar(255),
+	THRESHOLD_LEVEL varchar(255),
+	POPULATION_DENSITY varchar(255),
+	RESULT varchar(255),
+	INSPECTOR varchar(255),
+	DATETIME varchar(255),
+	COMMENT varchar(255)
+);
+
+create table results (
+	ANALYSIS_NO varchar(255),
+	STANDARD varchar(255),
+	ITEM varchar(255),
+	SIZE varchar(255),
+	VALUE float
+);
