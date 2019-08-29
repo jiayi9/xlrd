12b62e0b0791ea6d7ca2c128dfc7a788eebacdec
Jiayi Lu <215756347@qq.com>
2019-08-29 14:28:22 +0800

    Update read_and_insert.py

--- read_and_insert.py
+++ read_and_insert.py
@@ -1,17 +1,24 @@
-
 import xlrd
-import pandas as pd
-import numpy as np
 from datetime import datetime
+import pyodbc
+
+#############################################  Part 0: Set-up  #############################################
 
 loc = ("//bosch.com/dfsrb/DfsCN/loc/Wx/Dept/TEF/60_MFE_Manufacturing_Engineering/06_Data_Analytics/01_Project/RBCD/cleanliness/DNOX/2.2 -SM-2.xlsx") 
 #loc = ("/Users/tef-itm/Documents/cleanliness/xlrd/2.2 -SM-2.xlsx") 
 
+CONNECTION_STRING = (
+        'Driver={SQL Server};'
+        'Server=SGHZ001020351\SQLEXPRESS;'
+        'Database=CLEANLINESS;'
+        'Trusted_Connection=yes;'
+)
+
+conn = pyodbc.connect(CONNECTION_STRING)
+
 wb = xlrd.open_workbook(loc) 
 
 sheet = wb.sheet_by_index(0) 
-  
-sheet.cell_value(0,0)
 
 def col_to_index(col):
     return sum((ord(c) - 64) * 26**i for i, c in enumerate(reversed(col))) - 1
@@ -24,8 +31,21 @@ def format_datetime_2(x):
     x_as_datetime = datetime(*xlrd.xldate_as_tuple(x, wb.datemode))
     return(x_as_datetime.strftime("%Y-%m-%d %H:%M:%S"))
 
-
-
+def get_nrow(sheet, i = 33, j = 6):
+    N = 0
+    while True:
+        i = 33 + N
+        value = str(sheet.cell_value(i, 6))
+        print(value)
+        if value != '':
+            N = N + 1
+        else:
+            break
+    return(N)
+    
+#############################################  Part 1: Reports  #############################################
+    
+## define extraction cells
 Dictionary = [
 # General information
  ["Analysis_No", 7, "N"],
@@ -62,39 +82,19 @@ for index, item in enumerate(Dictionary):
     j = col_to_index(EXCEL_COL_INDEX)
     CONTENT = sheet.cell_value(i-1, j)
     Dictionary[index].append(CONTENT)
-#    print(NAME, i,j, CONTENT)
 
-pd.DataFrame(np.array(Dictionary),columns=['ProductType',"x","u",'xx'])
-
-Dictionary
-
+## format datetimes
+# Analysis_No
 Dictionary[0][3] = format_datetime_1(Dictionary[0][3])
-
+# Extraction_Date
 Dictionary[10][3] = format_datetime_2(Dictionary[10][3])
-
+# Datetime
 Dictionary[-2][3] = format_datetime_2(Dictionary[-2][3])
 
-
-
-import pyodbc
-conn = pyodbc.connect(
-        'Driver={SQL Server};'
-        'Server=SGHZ001020351\SQLEXPRESS;'
-        'Database=CLEANLINESS;'
-        'Trusted_Connection=yes;'
-        )
-
-
-cursor = conn.cursor()
-cursor.execute("select * from persons")
-
-for row in cursor:
-    print(row)
-
+# Connect 
 cursor = conn.cursor()
 
 VALUES = [str(x[3]) for x in Dictionary]
-
 sep = "','"
 VALUE_sep = sep.join(VALUES)
 STRING = "insert into REPORTS values ('" + VALUE_sep +"')"
@@ -103,42 +103,20 @@ cursor.execute(STRING)
 
 conn.commit()
 
-
-
-
-
-def get_nrow(sheet, i = 33, j = 6):
-    N = 0
-    while True:
-        i = 33 + N
-        value = str(sheet.cell_value(i, 6))
-        print(value)
-        if value != '':
-            N = N + 1
-        else:
-            break
-    return(N)
+#############################################  Part 2: Results  #############################################
+# Part 2: Results table
 
 N = get_nrow(sheet)
 
-
-#for i in range(N):
-#    print(sheet.cell_value(31, col_to_index('G')+i*2))
-
 COL_LABELS = [sheet.cell_value(31, col_to_index('G')+i*2) for i in range(N)]
-
 ROW_LABELS = ['Max', 'All', 'Length_Fibre', 'Length_Reflecting', 'Length_Others', 'Width_Fibre', 'Width_Reflecting', 'Width_Others']
 ROW_INDEX = [34, 35, 36, 37, 38, 39, 40, 41]
-
-
-
 
 L = list()
 for row_label, i in zip(ROW_LABELS, ROW_INDEX):
     for col_label, j in zip(COL_LABELS, range(N)):
-        
         value = sheet.cell_value(i - 1, col_to_index('G')+j*2)
-        print(i, j,row_label, col_label, value)
+#        print(i, j,row_label, col_label, value)
         L.append([row_label, col_label, value])
         
 ANALYSIS_NO = Dictionary[0][3]
@@ -150,5 +128,7 @@ for index, item in enumerate(L):
     cursor.execute(STRING, ANALYSIS_NO, STANDARD, item[0], item[1], item[2])
     conn.commit()
 
+#############################################  Close connection  #############################################
+conn.close()
 
 
